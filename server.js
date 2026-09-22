76e43a5b8e9949b8de0241e3b24c3754191ad4cd
Гадаборшев Ильяс И <student103@ithub.local>
2026-09-22 10:19:57 +0300

    q4

--- server.js
+++ server.js
@@ -5,19 +5,40 @@ const app = express()//запускаем express
 const PORT = 3000;// порт на котор будем запускать сервер
 
 
-app.get('/' ,(req , res)=>{
-    res.send("Главная страница")
-})
-
-app.get('/about' ,(req , res)=>{
-    res.send("Главная страница")
-})
-
-
-// const server = http.createServer((req, res)=>{// создаем сервер
-//     res.end("Hello World")//ответ клиенту когда он зайдет на наш сервер
-
+// app.get('/' ,(req , res)=>{
+//     res.send("Главная страница")
 // })
+
+// app.get('/about' ,(req , res)=>{
+//     res.send("Главная страница")
+// })
+
+
+
+
+app.get('/contacts' ,(req , res)=>{
+    res.send("контакты")
+})
+
+app.get('/product/:id' , (req  ,res)=>{
+    console.log(req.params.id);
+    
+    res.end(`Продукт id: ${req.params.id}`)
+})
+
+
+
+// app.get('/phone/:id' , (req  ,res)=>{
+//     console.log(req.params.id);
+    
+//     res.end(`phone id: ${req.params.id}`)
+// })
+
+app.post('/addproduct' , (req , res)=>{
+    const { name , age } = req.body;
+    
+})
+
 
 
 app.listen(PORT , ()=>{// тут расписываем на каком порту запускается сервер 
